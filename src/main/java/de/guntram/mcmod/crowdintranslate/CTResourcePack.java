a404b146a011e73533aaea802488cc50da10ed22
kerudion <119808157+kerudion@users.noreply.github.com>
2024-06-14 18:45:45 +0300

    Actually fixed 1.21 and made it build

--- src/main/java/de/guntram/mcmod/crowdintranslate/CTResourcePack.java
+++ src/main/java/de/guntram/mcmod/crowdintranslate/CTResourcePack.java
@@ -1,37 +1,45 @@
 package de.guntram.mcmod.crowdintranslate;
 
-import com.mojang.logging.LogUtils;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
-import java.util.function.Predicate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
 import net.minecraft.resource.InputSupplier;
 import net.minecraft.resource.ResourcePack;
+import net.minecraft.resource.ResourcePackInfo;
+import net.minecraft.resource.ResourcePackSource;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.resource.metadata.ResourceMetadataReader;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
 
 /**
  * Code taken from LambdAurora, oral permission on Discord on 2020-10-04
  */
 
-public class CTResourcePack implements ResourcePack
-{
+public class CTResourcePack implements ResourcePack {
     private final List<String> namespaces = new ArrayList<>();
     private static final Logger LOGGER = LogUtils.getLogger();
-    
+
     public CTResourcePack() {
-        for (String s: CrowdinTranslate.registeredMods()) {
-            put(s+"/lang");
+        for (String s : CrowdinTranslate.registeredMods()) {
+            put(s + "/lang");
         }
         put("crowdintranslate/lang");
     }
-    
-    public boolean put(String resource)
-    {
+
+    public boolean put(String resource) {
         Identifier id = fromPath(resource);
         if (!this.namespaces.contains(id.getNamespace())) {
             this.namespaces.add(id.getNamespace());
@@ -40,8 +48,7 @@ public class CTResourcePack implements ResourcePack
     }
 
     @Override
-    public InputSupplier<InputStream> openRoot(String ... fileName)
-    {
+    public InputSupplier<InputStream> openRoot(String... fileName) {
         File file = new File(CrowdinTranslate.getRootDir(), fileName[0]);
         if (file.exists()) {
             return InputSupplier.create(file.toPath());
@@ -51,64 +58,55 @@ public class CTResourcePack implements ResourcePack
     }
 
     @Override
-    public InputSupplier<InputStream> open(ResourceType type, Identifier id)
-    {
+    public InputSupplier<InputStream> open(ResourceType type, Identifier id) {
         return this.openRoot(type.getDirectory() + "/" + id.getNamespace() + "/" + id.getPath());
     }
 
     @Override
     public void findResources(ResourceType type, String namespace, String prefix, ResourcePack.ResultConsumer consumer) {
-        String start = CrowdinTranslate.getRootDir()+"/assets/" + namespace + "/" + prefix;
+        String start = CrowdinTranslate.getRootDir() + "/assets/" + namespace + "/" + prefix;
         String[] files = new File(start).list();
-        //LOGGER.info("finding resources for {} {}", namespace, prefix);
+        // LOGGER.info("finding resources for {} {}", namespace, prefix);
         if (files == null || files.length == 0) {
-            //LOGGER.info("found nothing");
+            // LOGGER.info("found nothing");
             return;
         }
-        //LOGGER.info("found {} files, first is {}", files.length, files[0]);
+        // LOGGER.info("found {} files, first is {}", files.length, files[0]);
         List<Identifier> resultList = Arrays.asList(files)
-                .stream()
-                .map(CTResourcePack::fromPath)
-                .collect(Collectors.toList());
-        for(Identifier result: resultList) {
-            //LOGGER.info("sending {} to consumer", result.toString());
+            .stream()
+            .map(CTResourcePack::fromPath)
+            .collect(Collectors.toList());
+        for (Identifier result : resultList) {
+            // LOGGER.info("sending {} to consumer", result.toString());
             consumer.accept(result, open(type, result));
         }
     }
 
     @Override
-    public Set<String> getNamespaces(ResourceType type)
-    {
+    public Set<String> getNamespaces(ResourceType type) {
         return new HashSet<>(this.namespaces);
     }
 
     @Override
-    public <T> T parseMetadata(ResourceMetadataReader<T> metaReader) throws IOException
-    {
+    public <T> T parseMetadata(ResourceMetadataReader<T> metaReader) throws IOException {
         return null;
     }
 
     @Override
-    public String getName()
-    {
-        return "CrowdinTranslate internal Resource Pack";
-    }
+    public void close() {}
 
-    @Override
-    public void close()
-    {
-    }
-    
-    @Override
-    public boolean isAlwaysStable() {
-        return true;
-    }
-
-    private static Identifier fromPath(String path)
-    {
+    private static Identifier fromPath(String path) {
         if (path.startsWith("assets/"))
             path = path.substring("assets/".length());
         String[] split = path.split("/", 2);
         return Identifier.of(split[0], split[1]);
+    }
+
+    @Override
+    public ResourcePackInfo getInfo() {
+        return new ResourcePackInfo("crowdintranslate",
+            Text.of("CrowdinTranslate internal Resource Pack"),
+            ResourcePackSource.NONE,
+            Optional.empty());
     }
 }
